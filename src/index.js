2c0a736e39fb31d427e55c143274acd646d6ed09
Elena <freshjs.ftm@gmail.com>
2021-11-09 12:06:52 +0200

    Add server webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+require('./style.css');
+const Component = require('./Component');
+
 console.log('test');
 const resultOfSumTwoNumber = 5+3;
 console.log(resultOfSumTwoNumber);
+const comp = new Component(12);
+console.log(comp.render());
+console.log('test');
+console.log('test');
